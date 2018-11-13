212b7d0747824dc6a76632fec6b18eedab3236bf
pummill <ben@casestack.io>
2018-11-13 10:32:44 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Blackjack Application v1.0
+
+
+
+//Card Variables
+
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'],
+    values = ['Ace', 'King', 'Queen', 'Jack', 'Ten',
+              'Nine', 'Eight', 'Seven', 'Six', 'Five',
+              'Four', 'Three', 'Two']
+
+//DOM Variables
+
+// Game Variables
